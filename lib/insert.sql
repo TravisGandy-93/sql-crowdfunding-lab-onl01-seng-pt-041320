2e8e461328646ea95a43b08acc48189a02e58da2
Travis Gandy <travis_gandy@yahoo.com>
2020-06-24 16:15:40 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -3,7 +3,7 @@ INSERT INTO users (name, age) VALUES ('Bear', 6);
 INSERT INTO users (name, age) VALUES ('Iguana', 4);
 INSERT INTO users (name, age) VALUES ('Alex', 33);
 INSERT INTO users (name, age) VALUES ('Amanda', 24);
-INSERT INTO users (name, age) VALUES ();
+INSERT INTO users (name, age) VALUES ('Sophie', 24);
 INSERT INTO users (name, age) VALUES ();
 INSERT INTO users (name, age) VALUES ();
 INSERT INTO users (name, age) VALUES ();
